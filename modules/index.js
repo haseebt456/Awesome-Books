36e11b330bf54b05dc350e42a9102d7d61225dbf
haseebt456 <SP22-BSE-075@CUILAHORE.EDU.PK>
2023-11-26 15:00:46 +0500

    added book class definition in index.js

--- modules/index.js
+++ modules/index.js
@@ -1,5 +1,12 @@
-import books from './book'
+class Books {
+    constructor(is,title,author)
+    {
+        this.id=id;
+        this.title=title;
+        this.author=authos;
+    }
+}
 
 function addBooks (){
-    
+
 }
