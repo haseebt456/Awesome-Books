0da157bb7987e73d272f26ce75513b595d726786
haseebt456 <SP22-BSE-075@CUILAHORE.EDU.PK>
2023-11-26 17:03:11 +0500

    writted an add book funtion

--- modules/index.js
+++ modules/index.js
@@ -12,6 +12,20 @@ class bookManagement {
     static myBooks=[];
     static addBooks(book){
         const bookDiv=document.createElement('div');
-        
+        bookDiv.classList.add('.book');
+        const bookTitle= document.createAttribute('h2');
+        const bookAuthor = document.createElement('h3');
+        const removeBtn = document.createElement('button');
+        removeBtn.classList.add('remove');
+        bookTitle.textContent = book.title;
+        bookAuthor.textContent = book.author;
+        removeBtn.textContent = 'remove';
+        bookDiv.appendChild(bookTitle);
+        bookDiv.appendChild(bookAuthor);
+        bookDiv.appendChild(removeBtn);
+        container.appendChild(bookDiv);
+
+        const bookJSON = JSON.stringify(this.myBooks);
+        localStorage.setItem('book',bookJSON);
     }
 }
